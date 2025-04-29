01d578d63bc27188dbefb23e80d8045a374b7810
Duja Abdurhman <134400247+DujaAbdulrhman@users.noreply.github.com>
2025-04-29 22:23:19 +0300

    Update UserDTO.java
    deleted

--- src/main/java/com/example/project3/DTO/UserDTO.java
+++ src/main/java/com/example/project3/DTO/UserDTO.java
@@ -1,14 +1,1 @@
-package com.example.project3.DTO;
-
-
-import lombok.*;
-
-@Getter @Setter @AllArgsConstructor @NoArgsConstructor
-public class UserDTO {
-
-    private Integer id;
-    private String username;
-    private String name;
-    private String email;
-    private String role;
-}
+I DELETE IT
